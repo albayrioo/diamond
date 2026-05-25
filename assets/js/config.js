db8f6e6de7f8afb3f4d7164088202188869ba148
albayrioo <ozkagsr@gmail.com>
2026-05-25 19:53:18 +0300

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+window.SITE_CONFIG = {
+  companyName: 'GARENT KURUMSAL FİLO',
+  phoneDisplay: '905521093310',
+  phoneRaw: '905521093310',
+  whatsappRaw: '905530848434',
+  whatsappMessage: 'Merhaba, araç rezervasyonu yapmak istiyorum.',
+  locations: ['İstanbul Havalimanı', 'Sabiha Gökçen', 'Taksim', 'Kadıköy', 'Beşiktaş', 'Bursa Merkez'],
+  workingHours: '7/24 teslimat ve rezervasyon desteği'
+};
